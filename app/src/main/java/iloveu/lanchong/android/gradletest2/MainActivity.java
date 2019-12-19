811ef9e4176de7eb25fca4e0bdd92b016460f62b
zhangj52 <zhangj52@ziroom.com>
2019-12-19 10:13:25 +0800

    首次提交

---
+++ app/src/main/java/iloveu/lanchong/android/gradletest2/MainActivity.java
@@ -0,0 +1,74 @@
+package iloveu.lanchong.android.gradletest2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import iloveu.lanchong.android.game.GameActivity;
+
+
+/**
+ * Build Variants使得
+ *
+ * 界面展示不同
+ * 跳转页面不同
+ * 
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                                startActivity(intent);
+                            }
+                        }).show();
+            }
+        });
+        ((TextView)findViewById(R.id.appid)).setText(BuildConfig.APPLICATION_ID);
+        ((TextView)findViewById(R.id.flavor)).setText(BuildConfig.FLAVOR);
+        ((TextView)findViewById(R.id.game)).setText(BuildConfig.GAME);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
